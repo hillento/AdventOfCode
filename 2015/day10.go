48affe99bebc7d6f415280295a4d58f913964ede
Alex Brown <brown.alex.tyler@gmail.com>
2025-06-18 21:54:34 -0600

    cleanup

--- 2015/day10.go
+++ 2015/day10.go
@@ -8,11 +8,10 @@ import (
 func Day10(input string, itr int) {
 	in := strings.TrimSpace(input)
 
-	for i := 0; i < itr; i++ {
+	for range itr {
 		var count int
 		var seq strings.Builder
-
-		for i := 0; i < len(in); i++ {
+		for i := range len(in) {
 			if i == len(in)-1 || in[i] != in[i+1] {
 				// add to seen
 				seq.WriteString(fmt.Sprintf("%d%s", count+1, string(in[i])))
@@ -21,10 +20,8 @@ func Day10(input string, itr int) {
 				// build up running count
 				count++
 			}
-
 		}
 		in = seq.String()
 	}
-
 	fmt.Printf("Day 10 - %d LookSay transformations: %d\n", itr, len(in))
 }
