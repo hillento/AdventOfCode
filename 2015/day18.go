f9422c46b94a9eef2f6d0b18b8dba40293fff66c
Alex Brown <brown.alex.tyler@gmail.com>
2025-07-09 22:04:59 -0600

    changing grid value

--- 2015/day18.go
+++ 2015/day18.go
@@ -7,82 +7,89 @@ import (
 
 func Day18(input string, steps int) {
 	lines := strings.Split(strings.TrimSpace(input), "\n")
-	mat := make([][]string, len(lines))
+	mat := make([][]rune, len(lines))
+
+	ON := '#'
+	OFF := '.'
 
 	for i, line := range lines {
-		mat[i] = strings.Split(line,`''`)
+		mat[i] = []rune(line)
 	}
 
 	totalOn := 0
-	for range steps {
+	for range 1 {
 		newMat := mat
 		newMatOn := 0
 
 		for i := 0; i < len(mat); i++ {
 			for j := 0; j < len(mat[0]); j++ {
-				onCount := 0
-				if i != 0 {
-					if j != 0 {
-						if mat[i-1][j-1] == "#" {
-							onCount++
-						}
-					}
-					if mat[i-1][j] == "#" {
-						onCount++
-					}
-					if j != len(mat[0]) -1 {
-						if mat[i-1][j+1] == "#" {
-							onCount++
-						}
-					}
-				}
-
+				fmt.Printf("Coordinate: (%d, %d) is a %s\n", i, j, string(mat[i][j]))
 				if j != 0 {
-					if mat[i][j-1] == "#" {
+					fmt.Printf("Coordinate: (%d, %d) is a %s\n", i, j-1, string(mat[i][j-1]))
+				}
+				onCount := 0
+
+				if i != 0 {
+					if j != 0 && mat[i-1][j-1] == ON {
+						fmt.Println(i-1, j-1)
+						onCount++
+					}
+					if mat[i-1][j] == ON {
+						fmt.Println(i-1, j)
+						onCount++
+					}
+					if j != len(mat[0])-1 && mat[i-1][j+1] == ON {
+						fmt.Println(i-1, j+1)
 						onCount++
 					}
 				}
 
-				if j != len(mat[0]) -1 {
-					if mat[i][j+1] == "#" {
-						onCount++
-					}
+				if j != 0 && mat[i][j-1] == ON {
+					fmt.Println(i, j-1)
+					onCount++
 				}
 
-				if i != len(mat) -1 {
-					if j != 0 {
-						if mat[i+1][j-1] == "#" {
-							onCount++
-						}
-					}
-					if mat[i+1][j] == "#" {
+				if j != len(mat[0])-1 && mat[i][j+1] == ON {
+					fmt.Println(i, j+1)
+					onCount++
+				}
+
+				if i != len(mat)-1 {
+					if j != 0 && mat[i+1][j-1] == ON {
+						fmt.Println(i+1, j-1)
 						onCount++
 					}
-					if j != len(mat[0]) - 1 {
-						if mat[i+1][j+1] == "#" {
-							onCount++
-						}
+					if mat[i+1][j] == ON {
+						fmt.Println(i+1, j)
+						onCount++
+					}
+					if j != len(mat[0])-1 && mat[i+1][j+1] == ON {
+						fmt.Println(i+1, j+1)
+						onCount++
 					}
 				}
-				if onCount == 3 && mat[i][j] == "." {
-					newMat[i][j] = "#"
+				if onCount == 3 && mat[i][j] == OFF {
+					newMat[i][j] = '#'
 					newMatOn++
-				} else if onCount == 2 && mat[i][j] == "#" {
-					newMat[i][j] = "."
-				} else if onCount == 3 && mat[i][j] == "#" {
-					newMat[i][j] = "."
+				} else if onCount == 2 && mat[i][j] == ON {
+					newMat[i][j] = '#'
+				} else if onCount == 3 && mat[i][j] == ON {
+					newMat[i][j] = '#'
+				} else {
+					newMat[i][j] = '.'
 				}
+				fmt.Println("Neighbors on: ", onCount)
 			}
 		}
 		mat = newMat
-		for _, v := range mat {
-			fmt.Println(v)
-		}
-		fmt.Println(totalOn)
+		// for _, v := range mat {
+		// 	fmt.Println(string(v))
+		// }
+		// fmt.Println(totalOn)
 		totalOn = newMatOn
 	}
 	for _, v := range mat {
-		fmt.Println(v)
+		fmt.Println(string(v))
 	}
 	fmt.Println(totalOn)
 }
